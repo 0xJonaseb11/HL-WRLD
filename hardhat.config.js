596967203e0f6e16c6844cb1211a086b0e35861a
jonas-sebera <sebejaz99@gmail.com>
2023-11-03 16:37:46 +0200

    init refreshed

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
     hardhat: {},
     sepolia: {
       url: "https://eth-sepolia.g.alchemy.com/v2/qZFTwEtghVmUO0U2AEEPldBA7w_9d6D0",
-      accounts: [`0x${PRIVATE_KEY}`],
+      accounts: [`0x${PRIVATE_KEY}`]
     },
   },
 };
