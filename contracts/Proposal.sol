1a280dec62b42738fa858eb3bf0fb77c8dd9dbbd
Jaz-3-0 <sebejaz99@gmail.com>
2024-02-12 19:30:44 +0200

    calculate current state

--- contracts/Proposal.sol
+++ contracts/Proposal.sol
@@ -90,6 +90,27 @@ contract ProposalContract {
           votedAddresses = [owner];
        }
     }   
+
+    // calculate current state
+    function calculateCurrentState() private view returns(bool) {
+        Proposal storage proposal = proposal_history[counter];
+
+        uint256 approve = proposal.approve;
+        uint256 reject = proposal.reject;
+        uint256 pass = proposal.pass;
+
+        // logic
+        if (proposal.pass % 2 == 1) {
+            pass += 1;
+        }
+        pass /= 2;
+
+        if (approve > reject + pass) {
+            return true;
+        } else {
+            return false;
+        }
+    }
    
   
 }
